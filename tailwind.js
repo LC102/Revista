bba782804451955f9879f971fe92b65f8b1880fc
LC102 <leoemmatav2002@gmail.com>
2025-05-18 17:56:36 -0600

    Primer commit: Proyecto Laravel con Tailwind + Flowbite

---
+++ tailwind.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php", // Archivos Blade
+    "./resources/**/*.js",        // Archivos JavaScript
+    "./resources/**/*.vue",       // Si usas Vue
+    "./node_modules/flowbite/**/*.js" // Componentes de Flowbite
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin'),    // Plugin de Flowbite
+    require('@tailwindcss/forms')  // Estilos para formularios (opcional)
+  ]
+}
