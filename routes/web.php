1ea7f186cd24fcdb0f3e123b51f566c56bc4ea80
LC102 <leoemmatav2002@gmail.com>
2025-05-18 18:51:36 -0600

    Agregué iniciocinco

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'pagina1'); 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::view('/inicio','pagina1');
 Route::view('/noticias','nivel2.pagina2');
