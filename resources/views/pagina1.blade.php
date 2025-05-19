219902b5e2426960b2017860f53f1372a7a0abc1
LC102 <leoemmatav2002@gmail.com>
2025-05-19 02:11:33 -0600

    Agregué m

--- resources/views/pagina1.blade.php
+++ resources/views/pagina1.blade.php
@@ -1,17 +1,6 @@
 @extends('layout/base')
 
-@section('contenido')
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inicio</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-    
+@section('contenido')    
 
 <body>
     
